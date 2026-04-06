35e3641a8f9d82e29ecea52c9e6a632dd02e5e08
Thiago Andrade <thiago.irias.64@gmail.com>
2026-04-06 09:05:22 -0300

    calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -16,32 +16,32 @@ import math
 num1 = float(input('Digite o primeiro número: '))
 num2 = float(input('Digite o segundo número: '))
 
-print("+ (adição)")
-print('- (subtração)')
-print('* (multiplicação)')
-print('/ (divisão)')
-print('^ (potência)')
-print('raiz')
-print('par/impar')
+print("1 (adição)")
+print('2 (subtração)')
+print('3 (multiplicação)')
+print('4 (divisão)')
+print('5 (potência)')
+print('6 (raiz)')
+print('7 (par/impar)')
 
 operacao = input('Digite a operação desejada: ')
 
-if operacao == '+':
+if operacao == '1':
     print(f'O resultado da somoa é {num1 + num2:.2f}')
-elif operacao == '-':
+elif operacao == '2':
     print(f'O resultado da subtração é {num1 - num2:.2f}')
-elif operacao == '*':
+elif operacao == '3':
     print(f'O resultado da multiplicação é {num1 * num2:.2f}')
-elif operacao == '/':
+elif operacao == '4':
     print(f'O resultado da divisão é {num1 / num2:.2f}')
-elif operacao == '^':
+elif operacao == '5':
     pot = float(input('Qual potencia quer elevar ? '))
     print(f'O resultado da potencia do primeiro número é {pow(num1, pot):.2f}')
     print(f'O resultado da potencia do segundo número é {pow(num2, pot):.2f}')
-elif operacao == 'raiz':
+elif operacao == '6':
     print(f'A raiz quadrada do primeiro número é {math.sqrt(num1):.2f}')
     print(f'A raiz quadrada do segundo número é {math.sqrt(num2):.2f}')
-elif operacao == 'par/impar':
+elif operacao == '7':
     if num1 % 2 == 0:
         print('O primeiro número é par')
     else:
